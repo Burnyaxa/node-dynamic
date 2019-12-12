4595dc4315ce26a9b2a54f252871b9bce55cd188
Artem Supryhan <suprigan.artem1@gmail.com>
2019-12-12 11:48:57 +0200

    Fix code style

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const catsRoutes = require('./routes/cats');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
 });
 
 app.engine('hbs', hbs.engine);
+
 app.set('view engine', 'hbs');
 app.set('views', 'views');
-app.use(express.urlencoded({ extended: true }));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(catsRoutes);
+
 async function start() {
     try {
         await mongoose.connect(
