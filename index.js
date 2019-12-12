23c10c5ac1306700dbe0b921e3b8011e4b5f82d8
Artem Supryhan <suprigan.artem1@gmail.com>
2019-12-12 02:45:46 +0200

    Added Heroku files

--- index.js
+++ index.js
@@ -6,29 +6,31 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs'
+    defaultLayout: 'main',
+    extname: 'hbs',
 });
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(catsRoutes);
-async function start(){
-  try {
-    await mongoose.connect('mongodb+srv://Burnyaxa:admin1337@cluster0-s4uhh.mongodb.net/cats', {
-      useNewUrlParser: true,
-      useFindAndModify: false
-    });
-    app.listen(PORT, () => {
-        console.log('Server running at ' + PORT);
-    });
-  }
-  catch (e) {
-    console.log(e);
-  }
+async function start() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://Burnyaxa:admin1337@cluster0-s4uhh.mongodb.net/cats',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: false,
+            }
+        );
+        app.listen(PORT, () => {
+            console.log('Server running at ' + PORT);
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 start();
